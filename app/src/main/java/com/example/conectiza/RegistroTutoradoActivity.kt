11c9a03288e20f07bda2436798f37bc655f05a43
Giselle Carmona Cid <118147811+GiselleCarCid@users.noreply.github.com>
2025-01-08 04:29:56 -0600

    Add files via upload

---
+++ app/src/main/java/com/example/conectiza/RegistroTutoradoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.conectiza
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistroTutoradoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.registrotutorado)
+    }
+}
