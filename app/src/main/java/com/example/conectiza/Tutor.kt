2287ac910b2e0391440da311aaf5749893a81508
Ricardo Mora <rurimoca@gmail.com>
2025-01-09 01:09:12 -0600

    Elimino paquetes no utilizados

--- app/src/main/java/com/example/conectiza/Tutor.kt
+++ app/src/main/java/com/example/conectiza/Tutor.kt
@@ -1,6 +1,5 @@
 package com.example.conectiza
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,21 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.*
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.material3.Icon as Icon1
+import com.example.conectiza.components.BottomNavigationBar
 import com.example.conectiza.ui.theme.ConectizaTheme
 
 class Tutor : ComponentActivity() {
