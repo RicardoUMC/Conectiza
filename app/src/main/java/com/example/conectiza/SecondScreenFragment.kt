d6f061f28390a332757bc73f4a46c749879343c9
Ricardo Mora <rurimoca@gmail.com>
2025-01-09 15:09:35 -0600

    Elimina proyecto

--- app/src/main/java/com/example/conectiza/SecondScreenFragment.kt
+++
@@ -1,4 +0,0 @@
-package com.example.conectiza
-import androidx.fragment.app.Fragment
-
-class SecondScreenFragment : Fragment(R.layout.bienvenida2)
--- app/src/main/java/com/example/conectiza/ThirdScreenFragment.kt
+++
@@ -1,4 +0,0 @@
-package com.example.conectiza
-import androidx.fragment.app.Fragment
-
-class ThirdScreenFragment : Fragment(R.layout.bienvenida3)
